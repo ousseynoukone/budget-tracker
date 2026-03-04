c967becf89540f04341ff0f946910ac680d9b8e3
ousseynoukone <ousseynou781227@gmail.com>
2026-03-04 12:08:28 +0100

    Transaction List

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
-import { useState } from 'react'
 import './App.css'
 import TransactionForm from './components/TransactionForm'
+import TransactionList from './components/TransactionList'
+import { TransactionType, type Transaction } from './model/transaction'
 
 function App() {
+  const sampleTransactions: Transaction[] = [
+    {
+      title: 'Salaire',
+      amount: 2500,
+      type: TransactionType.Deposit,
+      date: new Date('2024-03-01T09:00'),
+    },
+    {
+      title: 'Loyer',
+      amount: 900,
+      type: TransactionType.Withdrawal,
+      date: new Date('2024-03-03T10:30'),
+    },
+  ]
+
   return (
     <>
 
     <TransactionForm/>
+    <TransactionList transactions={sampleTransactions} />
     </>
   )
 }
